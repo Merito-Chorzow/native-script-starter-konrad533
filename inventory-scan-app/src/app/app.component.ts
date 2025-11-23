87c5458b65364eb8c610822def7871120f72bec4
konsil53 <konradkrz123@gmail.com>
2025-11-23 14:15:36 +0100

    Konfiguracja startu aplikacji

--- inventory-scan-app/src/app/app.component.ts
+++ inventory-scan-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { PageRouterOutlet } from '@nativescript/angular';
+import { NativeScriptRouterModule } from '@nativescript/angular';
 
 @Component({
   selector: 'ns-app',
-  templateUrl: './app.component.html',
-  imports: [PageRouterOutlet],
+  standalone: true,
+  // templateUrl: './app.component.html',
+  template: `<page-router-outlet></page-router-outlet>`,
+  imports: [NativeScriptRouterModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppComponent {}
